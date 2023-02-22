2426670d214b5538b99bb11109cb1cde33a3a57a
Amir Sohail <amirsohail335@gmail.com>
2023-02-22 10:37:09 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import CreateTweet from './components/CreateTweet';
 import Tweet from './components/Tweet';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div>
       <h1>Hello React</h1>
       <Tweet/>
+      <CreateTweet/>
       
     </div>
   );
---
+++ src/components/CreateTweet.js
@@ -0,0 +1,13 @@
+
+
+const CreateTweet = () => {
+    return (
+      <form>
+        <textarea cols="30" rows="10"></textarea>
+        <button>Submit</button>
+
+      </form>
+    );
+  };
+
+  export default CreateTweet;
